3fac7ea57c8d9f32b35052a19d3c675be15327a5
Albert Einstein <javastraat@hotmail.com>
2026-03-20 16:32:43 +0100

    flash batch fix

--- flash.sh
+++ flash.sh
@@ -1,5 +1,5 @@
 esptool.py --chip esp32 --port /dev/cu.usbserial-110 --baud 115200 write_flash -z \
-  0x1000 ulanzi-espnow.ino.bootloader.bin \
-  0x8000 ulanzi-espnow.ino.partitions.bin \
-  0xe000 boot_app0.bin \
-  0x10000 ulanzi-espnow.ino.bin
+  0x1000 build/esp32.esp32.esp32/ulanzi-espnow.ino.bootloader.bin \
+  0x8000 build/esp32.esp32.esp32/ulanzi-espnow.ino.partitions.bin \
+  0xe000 build/esp32.esp32.esp32/boot_app0.bin \
+  0x10000 build/esp32.esp32.esp32/ulanzi-espnow.ino.bin
