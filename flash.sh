2c76d24151fc208200746c3a965fe4db22c4ac3b
Albert Einstein <javastraat@hotmail.com>
2026-03-25 17:45:00 +0100

    wait for serial before flash

--- flash.sh
+++ flash.sh
@@ -1,4 +1,12 @@
-esptool.py --chip esp32 --port /dev/cu.usbserial-110 --baud 115200 write_flash -z \
+PORT=/dev/cu.usbserial-1130
+
+echo "Waiting for $PORT to be available..."
+while [ ! -e "$PORT" ]; do
+  sleep 1
+done
+echo "Port found, flashing..."
+
+esptool.py --chip esp32 --port "$PORT" --baud 115200 write_flash -z \
   0x1000 build/esp32.esp32.esp32/ulanzi-espnow.ino.bootloader.bin \
   0x8000 build/esp32.esp32.esp32/ulanzi-espnow.ino.partitions.bin \
   0xe000 build/esp32.esp32.esp32/boot_app0.bin \
